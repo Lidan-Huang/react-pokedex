bbac6cabf47070c1d85963d34e8dcd7d7c22bc7f
Lidan-Huang <huangld0314@gmail.com>
2022-10-05 16:33:01 -0700

    added Pokecard.css and fixing the presentation of pokecards

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,10 +1,19 @@
+import "./Pokecard.css";
+
 const BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
-function Pokecard({id, name, type, base_experience}) {
-  let url = `BASE_URL${id}.png`;
+function Pokecard({ id, name, type, base_experience }) {
+  let url = `${BASE_URL}${id}.png`;
+
+  let style = {
+    backgroundImage: `url(${url})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: 'grey',
+    backgroundPosition: 'center',
+  };
   return (
-    <div style={{backgroundImage: url}}>
-      <h4>{name}</h4>
+    <div style={style} className="Pokecard">
+      <h4 className="Pokecard name">{name}</h4>
       <p>Type: {type}</p>
       <p>EXP: {base_experience}</p>
     </div>
